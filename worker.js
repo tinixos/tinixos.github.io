c1c83a0147a9cedf47804f43f3e58c562ad51ce8
张建涛 <jiantaozhang.zjt@alipay.com>
2016-05-10 16:42:03 +0800

    fix registers info

--- worker.js
+++ worker.js
@@ -65,51 +65,145 @@ var progress_ticks = 0;
 
 function show_progress(e)
 {
-    var el = $("status");
-    //el.style.display = "block";
+    var per100 = Math.floor(e.loaded / e.total * 100);
+
+    $("process").style.display = "block";
+    
+    $("image_name").textContent = (e.th+1).toString() + "/" + (e.sh+1).toString() + "  " + e.uh;
+    $("image_size").textContent = (e.loaded).toString() + " / " + (e.total).toString();
+    $("load_process").textContent = per100.toString() + "% ";
 
     if(e.th === e.sh - 1 && e.loaded >= e.total - 2048)
     {
-        // last file is (almost) loaded
-        el.innerHTML = "Download completed.";
+        $("process").style.display = "none";
         return;
     }
 
-    var line = "Downloading images " + e.th;
-
-    if(typeof e.th === "number" && e.sh)
-    {
-        line += "[" + (e.th + 1) + "/" + e.sh + "] ";
-    }
-
-    if(e.total && typeof e.loaded === "number")
-    {
-        var per100 = Math.floor(e.loaded / e.total * 100);
-        per100 = Math.min(100, Math.max(0, per100));
-
-        var per50 = Math.floor(per100 / 2);
-
-        line += per100 + "%";
-    }
-    else
-    {
-        line += chr_repeat(".", progress_ticks++ % 50);
-    }
-
-    el.innerHTML = line;
 }
 
 function init_ui(emulator)
 {
 
-    function toHex(num)
-    {//将一个数字转化成16进制字符串形式
-        return num<16?"0x0"+num.toString(16).toUpperCase():"0x"+num.toString(16).toUpperCase();
-    }
     var last_tick = 0;
     var running_time = 0;
     var last_instr_counter = 0;
     var interval;
+
+    /**
+    *
+    *  Javascript sprintf
+    *
+    *
+    **/
+
+    var sprintfWrapper = {
+      init : function () {
+        if (typeof arguments == "undefined") { return null; }
+        if (arguments.length < 1) { return null; }
+        if (typeof arguments[0] != "string") { return null; }
+        if (typeof RegExp == "undefined") { return null; }
+        var string = arguments[0];
+        var exp = new RegExp(/(%([%]|(\-)?(\+|\x20)?(0)?(\d+)?(\.(\d)?)?([bcdfosxX])))/g);
+        var matches = new Array();
+        var strings = new Array();
+        var convCount = 0;
+        var stringPosStart = 0;
+        var stringPosEnd = 0;
+        var matchPosEnd = 0;
+        var newString = '';
+        var match = null;
+        while (match = exp.exec(string)) {
+          if (match[9]) { convCount += 1; }
+          stringPosStart = matchPosEnd;
+          stringPosEnd = exp.lastIndex - match[0].length;
+          strings[strings.length] = string.substring(stringPosStart, stringPosEnd);
+          matchPosEnd = exp.lastIndex;
+          matches[matches.length] = {
+            match: match[0],
+            left: match[3] ? true : false,
+            sign: match[4] || '',
+            pad: match[5] || ' ',
+            min: match[6] || 0,
+            precision: match[8],
+            code: match[9] || '%',
+            negative: parseInt(arguments[convCount]) < 0 ? true : false,
+            argument: String(arguments[convCount])
+          };
+        }
+        strings[strings.length] = string.substring(matchPosEnd);
+        if (matches.length == 0) { return string; }
+        if ((arguments.length - 1) < convCount) { return null; }
+        var code = null;
+        var match = null;
+        var i = null;
+        var substitution;
+        for (i=0; i<matches.length; i++) {
+          if (matches[i].code == '%') { substitution = '%' }
+          else if (matches[i].code == 'b') {
+            matches[i].argument = String(Math.abs(parseInt(matches[i].argument)).toString(2));
+            substitution = sprintfWrapper.convert(matches[i], true);
+          }
+          else if (matches[i].code == 'c') {
+            matches[i].argument = String(String.fromCharCode(parseInt(Math.abs(parseInt(matches[i].argument)))));
+            substitution = sprintfWrapper.convert(matches[i], true);
+          }
+          else if (matches[i].code == 'd') {
+            matches[i].argument = String(Math.abs(parseInt(matches[i].argument)));
+            substitution = sprintfWrapper.convert(matches[i]);
+          }
+          else if (matches[i].code == 'f') {
+            matches[i].argument = String(Math.abs(parseFloat(matches[i].argument)).toFixed(matches[i].precision ? matches[i].precision : 6));
+            substitution = sprintfWrapper.convert(matches[i]);
+          }
+          else if (matches[i].code == 'o') {
+            matches[i].argument = String(Math.abs(parseInt(matches[i].argument)).toString(8));
+            substitution = sprintfWrapper.convert(matches[i]);
+          }
+          else if (matches[i].code == 's') {
+            matches[i].argument = matches[i].argument.substring(0, matches[i].precision ? matches[i].precision : matches[i].argument.length)
+            substitution = sprintfWrapper.convert(matches[i], true);
+          }
+          else if (matches[i].code == 'x') {
+            matches[i].argument = String(Math.abs(parseInt(matches[i].argument)).toString(16));
+            substitution = sprintfWrapper.convert(matches[i]);
+          }
+          else if (matches[i].code == 'X') {
+            matches[i].argument = String(Math.abs(parseInt(matches[i].argument)).toString(16));
+            substitution = sprintfWrapper.convert(matches[i]).toUpperCase();
+          }
+          else {
+            substitution = matches[i].match;
+          }
+          newString += strings[i];
+          newString += substitution;
+        }
+        newString += strings[i];
+        return newString;
+      },
+      convert : function(match, nosign){
+        if (nosign) {
+          match.sign = '';
+        } else {
+          match.sign = match.negative ? '-' : match.sign;
+        }
+        var l = match.min - match.argument.length + 1 - match.sign.length;
+        var pad = new Array(l < 0 ? 0 : l).join(match.pad);
+        if (!match.left) {
+          if (match.pad == "0" || nosign) {
+            return match.sign + pad + match.argument;
+          } else {
+            return pad + match.sign + match.argument;
+          }
+        } else {
+          if (match.pad == "0" || nosign) {
+            return match.sign + match.argument + pad.replace(/0/g, ' ');
+          } else {
+            return match.sign + match.argument + pad;
+          }
+        }
+      }
+    }
+    var sprintf = sprintfWrapper.init;
 
     function update_info()
     {
@@ -129,24 +223,22 @@ function init_ui(emulator)
         $("running_time").textContent = time2str(running_time / 1000 | 0);
 
         //register
-        $("eip").textContent = emulator.get_statistics()["eip"];
+        $("eip").textContent = sprintf("0x%08X", emulator.get_statistics()["eip"] >= 0 ? emulator.get_statistics()["eip"] : emulator.get_statistics()["eip"] + 0x100000000);
         $("cpl").textContent = emulator.get_statistics()["cpl"] == 0 ? "kernel" : "user";
-        $("eflags").textContent = emulator.get_statistics()["flags"];
+        $("eflags").textContent = sprintf("0x%08X", emulator.get_statistics()["flags"] >= 0 ? emulator.get_statistics()["flags"] : emulator.get_statistics()["flags"] + 0x100000000);
         $("paging").textContent = emulator.get_statistics()["paging"] ? "true" : "false";
         $("protect").textContent = emulator.get_statistics()["protected_mode"] ? "true" : "false";
 
         var registers = emulator.get_statistics()["registers"];
 
-        $("eax").textContent = registers[reg_eax];
-        $("ebx").textContent = registers[reg_ebx];
-        $("ecx").textContent = registers[reg_ecx];
-        $("edx").textContent = registers[reg_edx];
-        $("esp").textContent = registers[reg_esp];
-        $("ebp").textContent = registers[reg_ebp];
-        $("esi").textContent = registers[reg_esi];
-        $("edi").textContent = registers[reg_edi];
-
-
+        $("eax").textContent = sprintf("0x%08X", registers[reg_eax] >= 0 ? registers[reg_eax] : registers[reg_eax] + 0x100000000);
+        $("ebx").textContent = sprintf("0x%08X", registers[reg_ebx] >= 0 ? registers[reg_ebx] : registers[reg_ebx] + 0x100000000);
+        $("ecx").textContent = sprintf("0x%08X", registers[reg_ecx] >= 0 ? registers[reg_ecx] : registers[reg_ecx] + 0x100000000);
+        $("edx").textContent = sprintf("0x%08X", registers[reg_edx] >= 0 ? registers[reg_edx] : registers[reg_edx] + 0x100000000);
+        $("esp").textContent = sprintf("0x%08X", registers[reg_esp] >= 0 ? registers[reg_esp] : registers[reg_esp] + 0x100000000);
+        $("ebp").textContent = sprintf("0x%08X", registers[reg_ebp] >= 0 ? registers[reg_ebp] : registers[reg_ebp] + 0x100000000);
+        $("esi").textContent = sprintf("0x%08X", registers[reg_esi] >= 0 ? registers[reg_esi] : registers[reg_esi] + 0x100000000);
+        $("edi").textContent = sprintf("0x%08X", registers[reg_edi] >= 0 ? registers[reg_edi] : registers[reg_edi] + 0x100000000);
     }
 
     emulator.add_listener("emulator-started", function()
