373297064cb7785c1bf8d94303eef72d26610b99
zhangjiantao <jiantaozhang@live.com>
2017-07-05 23:13:57 +0800

    update img url

--- worker.js
+++ worker.js
@@ -23,7 +23,7 @@ window.onload = function()
     var autostart = query_args["autostart"];
     
     if (typeof(image) == "undefined") {
-        img = "images/tinix.img";
+        img = "https://github.com/tinixos/tinix/raw/master/floppy.img";
     } else if (image.indexOf(".img") > 0) {
         img = image;
     } else if (image.indexOf(".iso") > 0) {
